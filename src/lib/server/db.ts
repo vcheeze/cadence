e135891696d9076046f78fe0ae9e72a7e5a82596
Peter Chen <manchesterutd7@gmail.com>
2024-06-06 15:20:23 +0400

    fix: manually override Prisma db url as temp fix

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,6 +1,14 @@
 import { PrismaClient } from '@prisma/client/edge';
 
-export const db = new PrismaClient();
+import { DATABASE_URL } from '$env/static/private';
+
+export const db = new PrismaClient({
+  datasources: {
+    db: {
+      url: DATABASE_URL
+    }
+  }
+});
 
 export interface DatabaseUser {
   id: string;
